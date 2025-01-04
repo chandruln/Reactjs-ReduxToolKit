92e1133a1c46acdd61c4f575b7475bebd5a09654
chandru <chan02ms@gmail.com>
2025-01-04 21:27:21 +0530

    Search keyboard

--- ReactProgram/src/Search/Search.js
+++ ReactProgram/src/Search/Search.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import './search.css';
 
 const Search = () => {
   const [searchText, setSearchText] = useState("")
   const [searchedData, setSearchedData] = useState([]);
   const [filtereddata, setFiltereddata] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(-1);
 
 //   const [datas, setDatas] = useState([
 //     { title: "text 1", },
@@ -26,21 +28,45 @@ const Search = () => {
     }else{
         setFiltereddata([])
     }
-  
-
   }, [searchText])
+
+  const handleKeyDown = (e) => {
+    if(selectedItem < filtereddata.length){
+        if(e.key === "ArrowUp" && selectedItem > 0){
+            setSelectedItem(prev => prev - 1);
+        } else if (e.key === "ArrowDown" && selectedItem < filtereddata.length - 1){
+            setSelectedItem(prev => prev + 1);
+        } else if (e.key == "Enter" && selectedItem >= 0){
+            window.open(filtereddata[selectedItem].images[0]);
+        }
+    } else {
+        setSelectedItem(-1);
+    }
+ 
+}
 
   return (
     <div>
         <h1>Search</h1>
-        <input type='text' autoComplete='off' value={searchText} onChange={(e) => setSearchText(e.target.value)} placeholder='Title..'/>
+        <input type='text' autoComplete='off'
+            value={searchText} 
+            onChange={(e) => setSearchText(e.target.value)} 
+            placeholder='Search Title..'
+            onKeyDown={handleKeyDown}
+        />
         <div>
             {
                 filtereddata
+                .slice(0,10)
                 .map((data, index) =>  
                     (
                         <>
-                            <a href={data.images[0]} key={index} target='_blank' style={{textDecoration: "none"}}>{data.title}</a>
+                            <a href={data.images[0]} 
+                                key={index} target='_blank' 
+                                style={{textDecoration: "none"}}
+                                className = {selectedItem === index ? 'selected active' : 'selected'}>
+                                {data.title}
+                            </a>
                             <br/>
                         </>
                     )
